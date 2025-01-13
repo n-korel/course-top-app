fc09f2034d13880f5e8a6534dde223d5eaa0876a
n-korel <n.korelskiy@hotmail.com>
2025-01-13 17:59:31 +0300

    Classnames

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { Button } from './components/Button/Button';
 import { Htag } from './components/Htag/Htag';
 
 export default function Home() {
 	return (
 		<>
 			<Htag tag="h1">Текст</Htag>
+			<Button appearance="primary">Кнопка</Button>
+			<Button appearance="ghost">Кнопка</Button>
 		</>
 	);
 }
