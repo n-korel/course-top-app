d015c3be09adc3e29f37fe38b7e741b32ae487a2
n-korel <n.korelskiy@hotmail.com>
2025-01-15 17:37:15 +0300

    Hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,43 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { Button } from './components/Button/Button';
 import { Htag } from './components/Htag/Htag';
+import { P } from './components/P/P';
+import { Tag } from './components/Tag/Tag';
 
 export default function Home() {
+	const [counter, setCounter] = useState<number>(0);
+
+	useEffect(() => {
+		console.log('Counter' + counter);
+		return function cleanup() {
+			console.log('Unmount');
+		};
+	}, []);
+
 	return (
 		<>
-			<Htag tag="h1">Текст</Htag>
-			<Button appearance="primary" arrow="right">
+			<Htag tag="h1">{counter}</Htag>
+			<Button appearance="primary" arrow="right" onClick={() => setCounter((x) => x + 1)}>
 				Кнопка
 			</Button>
 			<Button appearance="ghost" arrow="down">
 				Кнопка
 			</Button>
+			<P size="l">Большой</P>
+			<P>Средний</P>
+			<P size="s">Маленький</P>
+			<Tag size="s">Ghost</Tag>
+			<Tag size="s" color="green">
+				Green
+			</Tag>
+			<Tag size="m" color="red">
+				Red
+			</Tag>
+			<Tag size="s" color="primary">
+				Primary
+			</Tag>
 		</>
 	);
 }
